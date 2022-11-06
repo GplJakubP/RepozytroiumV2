ca5c52c330c65040f8fad4f9b574a42a93863b9c
w67173 <qsefthuko-1999@o.pl>
2022-11-06 13:40:33 +0100

    l1

--- Lekcja.py
+++ Lekcja.py
@@ -1,8 +1,12 @@
-for x in range(1, 100):
-    print(x)
-
-for x in range(1, 100):
-    print(x)
-
-    for x in range(7, 77 , 7):
-        print(x)
+for x in range(1, 101):
+    print(x, end=' , ')
+print('')
+for x in range(100,-1,-1):
+    print(x, end=' , ')
+print('')
+for x in range(7, 78 , 7):
+        print(x, end=' , ')
+print('')
+for x in range(20,-1,-2):
+    print(x, end=' , ')
+print('')
---
+++ Zadanie 8 lab 3.py
@@ -0,0 +1,5 @@
+x =int( input("wpisz lidzbę całkowitą" ))
+for j in range(x):
+    for i in range(x):
+        print("*",end=' ')
+    print()
