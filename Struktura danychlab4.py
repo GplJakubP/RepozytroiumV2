f8ee2f7aabbd00de5e4c571e149f87c25dc06ca8
w67173 <qsefthuko-1999@o.pl>
2022-11-06 14:19:08 +0100

    l1

---
+++ Struktura danychlab4.py
@@ -0,0 +1,46 @@
+"""
+listta_zakupów=['masło','chleb','banan','pasztet']
+print(listta_zakupów)
+print(listta_zakupów[0]+" ,"+listta_zakupów[1])
+listta_zakupów[2]="mleko"
+print(listta_zakupów)
+print(listta_zakupów[-2]+' ,'+listta_zakupów[-1])
+"""
+"""
+x = int(input("Podaj liczbe elementow: "))
+lista1 = []
+for i in range (x):
+    n = random.randint(1,10)
+    lista1.append(n)
+print(lista1)
+x1 = int(input("Podaj liczbe elementow dla drugiej listy: "))
+lista2 = []
+for i in range (x1):
+    n2 = random.randint(5,15)
+    lista2.append(n2)
+print(lista2)
+x3 = int(input("Podaj liczbe której szukasz: "))
+if x3 in lista1:
+    print("Liczba występuje w lista1")
+elif x3 in lista2:
+    print("Liczba występuje w lista2")
+else:
+    print("Nie ma takiej liczby w obu zestawach")
+"""
+"""
+print()
+zestaw_1_2 = zestaw_1 +zestaw_2
+zestaw_1_2.sort()
+print(zestaw_1_2)
+
+"""
+"""
+zwierzęta = []
+for x in range (4):
+    a = input('wpisz nazwy zwierząt ')
+    zwierzęta.append(a)
+print(zwierzęta)
+zwierzęta.sort()
+print(zwierzęta)
+
+"""
